7a58208180ce5390f8bee4da8f1087cb4edae1e6
Mahesh Shivaswamy <go2mahesh@gmail.com>
2018-05-12 12:11:55 -0700

    POC projects for COre and Angular 2

---
+++ ASP.NETCORE/Practical/Intro/ASP.NETWebApp/ASpNETCore2/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASpNETCore2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace ASpNETCore2.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
